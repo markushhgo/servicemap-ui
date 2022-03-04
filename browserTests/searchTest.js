10afb6b702f8968f7c7ff13a72f06573b584926d
juhomakkonen <juho.makkonen@turku.fi>
2022-03-04 11:05:21 +0200

    Update tests

--- browserTests/searchTest.js
+++ browserTests/searchTest.js
@@ -24,17 +24,11 @@ const searchUnits = async (t, search = 'uimastadion') => {
     .pressKey('ctrl+a delete')
     .typeText(input, search)
     .pressKey('enter');
-
-  // Get search list's data length
-  const searchView = ReactSelector('SearchView');
-  const unitCount = await searchView.getReact(({props}) => props.units.length);
-
-  return unitCount;
 }
 
 test('Navigate search view', async (t) => {
   // Test result orderer navigation
-  const unitCount = await searchUnits(t, 'kirjasto');
+  await searchUnits(t, 'kirjasto');
   const input = ReactSelector('WithStyles(ForwardRef(InputBase))').nth(0);
   let select =  ReactSelector('ResultOrderer WithStyles(ForwardRef(Select))');
 
@@ -110,10 +104,9 @@ test('Tab navigation works correctly', async (t) => {
 })
 
 test('Search does list results', async (t) => {
-  // const unitCount = await searchUnits(t);
-  const searchView = ReactSelector('SearchView')
+  const resultList = ReactSelector('ResultList')
   await t
-    .expect(searchView.getReact(({props}) => props.units.length)).gt(5, `Search didn't get results`);
+    .expect(resultList.getReact(({props}) => props.resultCount)).gt(0, `Search didn't get results`);
 });
 
 // Check that address search works and draws marker on map
@@ -160,27 +153,27 @@ test('ServiceItem click event takes to service page', async(t) => {
     .click(services.nth(0))
     .navigateTo(target);
 });
-
-test('Expanded suggestions does open and close correctly', async(t) => {
-  const button = await Selector('#ExpandSuggestions');
-  await t
-    .click(button);
+// Expanded suggestions are disabled for now
+// test('Expanded suggestions does open and close correctly', async(t) => {
+//   const button = await Selector('#ExpandSuggestions');
+//   await t
+//     .click(button);
   
-  const backButton = await ReactSelector('BackButton');
-  await t
-    .expect(backButton.focused).ok('Titlebar\'s back button should have focus')
-    // Go back to search view
-    .click(backButton);
+//   const backButton = await ReactSelector('BackButton');
+//   await t
+//     .expect(backButton.focused).ok('Titlebar\'s back button should have focus')
+//     // Go back to search view
+//     .click(backButton);
 
-  const viewText = await Selector(`#${viewTitleID}`).innerText;
-  const button2 = await Selector('#ExpandSuggestions');
-  await t
-    // Check that back button takes back to correct view
-    .expect(viewText).eql('Hakutulossivu', 'BackButton should take user back to search view')
-    // Check that focus is moved correctly when returning to search view
-    .expect(button2.focused).ok('ExpandSuggestions button should have focus');
+//   const viewText = await Selector(`#${viewTitleID}`).innerText;
+//   const button2 = await Selector('#ExpandSuggestions');
+//   await t
+//     // Check that back button takes back to correct view
+//     .expect(viewText).eql('Hakutulossivu', 'BackButton should take user back to search view')
+//     // Check that focus is moved correctly when returning to search view
+//     .expect(button2.focused).ok('ExpandSuggestions button should have focus');
 
-});
+// });
 
 test('SearchBar accessibility is OK', async(t) => {
 
@@ -254,14 +247,14 @@ test('ResultList accessibility attributes are OK', async(t) => {
 */
 });
 
-test('SuggestionButton accessibility attributes are OK', async(t) => {
-    // Check that ExapndSuggestions has correct accessibility attributes
-    const expandedSuggestionsButton = await Selector('#ExpandSuggestions');
-    const esbRole = await expandedSuggestionsButton.getAttribute('role')
-    await t
-      // We expect ExpandedSearchButton to have role link since it takes to another view 
-      .expect(esbRole).eql('link', 'ExpandedSearchButton should be considered a link');
-});
+// test('SuggestionButton accessibility attributes are OK', async(t) => {
+//     // Check that ExapndSuggestions has correct accessibility attributes
+//     const expandedSuggestionsButton = await Selector('#ExpandSuggestions');
+//     const esbRole = await expandedSuggestionsButton.getAttribute('role')
+//     await t
+//       // We expect ExpandedSearchButton to have role link since it takes to another view 
+//       .expect(esbRole).eql('link', 'ExpandedSearchButton should be considered a link');
+// });
 
 test('Tabs accessibility attributes are OK', async(t) => {
   await searchUnits(t, 'kirjasto');
@@ -277,9 +270,9 @@ test('Tabs accessibility attributes are OK', async(t) => {
 
 test('Search has aria-live element', async(t) => {
   await searchUnits(t, 'kirjasto');
-  const view = ReactSelector('SearchView');
-  const units = await view.getReact(({props}) => props.units ? props.units : []);
-  const unitCount = await units.filter(unit => unit.object_type === 'unit').length
+  const resultList = ReactSelector('PaginatedList')
+  const results = await resultList.getReact(({props}) => props.data || []);
+  const unitCount = await results.filter(result => result.object_type === 'unit').length
   const searchInfo = Selector('.SearchInfo').child(0);
   const siText = await searchInfo.innerText;
   const siAriaLive = await searchInfo.getAttribute('aria-live');
