603a4a6b50a75607ae95479cc12bb67c64d91e70
juhomakkonen <juho.makkonen@turku.fi>
2022-03-16 15:16:23 +0200

    Update trackerUrl

--- src/utils/tracking.js
+++ src/utils/tracking.js
@@ -4,15 +4,18 @@ import config from '../../config';
 const externalTheme = config.themePKG;
 const isExternalTheme = !externalTheme || externalTheme === 'undefined' ? null : externalTheme;
 
-const piwik = `https://${config.matomoUrl}/piwik.min.js`;
-const matomo = `https://${config.matomoUrl}/matomo.js`;
+const sourceOptional = `https://${config.matomoUrl}/piwik.min.js`;
+const sourceDefault = `https://${config.matomoUrl}/matomo.js`;
+
+const trackerOptional = `https://${config.matomoUrl}/tracker.php`;
+const trackerDefault = `https://${config.matomoUrl}/matomo.php`;
 
 const matomoTracker = (config.matomoSiteId && config.matomoUrl)
   ? new MatomoTracker({
     urlBase: `https://${config.matomoUrl}`,
     siteId: config.matomoSiteId,
-    trackerUrl: `https://${config.matomoUrl}/tracker.php`, // optional, default value: `${urlBase}matomo.php`
-    srcUrl: isExternalTheme ? matomo : piwik, // optional, default value: `${urlBase}matomo.js`
+    trackerUrl: isExternalTheme ? trackerDefault : trackerOptional, // optional, default value: `${urlBase}matomo.php`
+    srcUrl: isExternalTheme ? sourceDefault : sourceOptional, // optional, default value: `${urlBase}matomo.js`
     disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
     linkTracking: false, // optional, default value: true
   }) : null;
