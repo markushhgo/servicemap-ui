34c4465ede8a3b5fcb82ffaef9f9ab7d5ce6e788
juhomakkonen <juho.makkonen@turku.fi>
2022-03-22 13:11:24 +0200

    Edit if statement and matomoUrl

--- src/utils/tracking.js
+++ src/utils/tracking.js
@@ -2,17 +2,17 @@ import MatomoTracker from '@datapunt/matomo-tracker-js';
 import config from '../../config';
 
 const externalTheme = config.themePKG;
-const isExternalTheme = !externalTheme || externalTheme === 'undefined' ? null : externalTheme;
+const isExternalTheme = !externalTheme || typeof externalTheme === 'undefined' ? null : externalTheme;
 
-const sourceOptional = `https://${config.matomoUrl}/piwik.min.js`;
-const sourceDefault = `https://${config.matomoUrl}/matomo.js`;
+const sourceOptional = `${config.matomoUrl}/piwik.min.js`;
+const sourceDefault = `${config.matomoUrl}/matomo.js`;
 
-const trackerOptional = `https://${config.matomoUrl}/tracker.php`;
-const trackerDefault = `https://${config.matomoUrl}/matomo.php`;
+const trackerOptional = `${config.matomoUrl}/tracker.php`;
+const trackerDefault = `${config.matomoUrl}/matomo.php`;
 
 const matomoTracker = (config.matomoSiteId && config.matomoUrl)
   ? new MatomoTracker({
-    urlBase: `https://${config.matomoUrl}`,
+    urlBase: `${config.matomoUrl}`,
     siteId: config.matomoSiteId,
     trackerUrl: isExternalTheme ? trackerDefault : trackerOptional, // optional, default value: `${urlBase}matomo.php`
     srcUrl: isExternalTheme ? sourceDefault : sourceOptional, // optional, default value: `${urlBase}matomo.js`
