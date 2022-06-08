4493bd101b2e1f2e32607dda05c7dd038d7fc021
juhomakkonen <juho.makkonen@turku.fi>
2022-06-08 10:55:52 +0300

    Display descriptions of route units

--- src/components/MobilityPlatform/CultureRouteUnits/CultureRouteUnits.js
+++ src/components/MobilityPlatform/CultureRouteUnits/CultureRouteUnits.js
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Typography } from '@material-ui/core';
+import { useIntl } from 'react-intl';
+import { useMap } from 'react-leaflet';
+import { Typography, ButtonBase } from '@material-ui/core';
+import { Close } from '@material-ui/icons';
 import routeUnitIcon from 'servicemap-ui-turku/assets/icons/icons-icon_culture_route.svg';
 import MobilityPlatformContext from '../../../context/MobilityPlatformContext';
 import { selectRouteName } from '../utils/utils';
@@ -9,7 +12,10 @@ import { selectRouteName } from '../utils/utils';
 const CultureRouteUnits = ({ classes, cultureRouteUnits }) => {
   const { cultureRouteId } = useContext(MobilityPlatformContext);
 
+  const intl = useIntl();
+
   const locale = useSelector(state => state.user.locale);
+  const map = useMap();
 
   const { Marker, Popup } = global.rL;
   const { icon } = global.L;
@@ -19,19 +25,57 @@ const CultureRouteUnits = ({ classes, cultureRouteUnits }) => {
     iconSize: [45, 45],
   });
 
+  const closePopup = () => {
+    if (map) {
+      map.closePopup();
+    }
+  };
+
   const activeCultureRouteUnits = cultureRouteUnits.filter(item => item.mobile_unit_group.id === cultureRouteId);
+
+  const renderDescription = (descriptionFi, descriptionEn, descriptionSv, nameFi, nameEn, nameSv) => {
+    let desc = descriptionFi;
+    let name = nameFi;
+    if (locale === 'sv') {
+      desc = descriptionSv;
+      name = nameSv;
+    } else if (locale === 'en') {
+      desc = descriptionEn;
+      name = nameEn;
+    }
+    if (!desc) {
+      return intl.formatMessage({ id: 'mobilityPlatform.content.description.notAvailable' });
+    }
+    return desc.replace(name, '');
+  };
 
   return (
     <>
       <div>
-        {activeCultureRouteUnits && activeCultureRouteUnits.length > 0
+        {activeCultureRouteUnits
+          && activeCultureRouteUnits.length > 0
           && activeCultureRouteUnits.map(item => (
             <Marker key={item.id} icon={customIcon} position={[item.geometry_coords.lat, item.geometry_coords.lon]}>
-              <Popup>
+              <Popup closeButton={false} maxHeight={400} className="culture-route-unit-popup">
                 <div className={classes.popupInner}>
-                  <div className={classes.subtitle}>
-                    <Typography variant="body2">
+                  <div className={classes.header}>
+                    <Typography variant="subtitle1">
                       {selectRouteName(locale, item.name, item.name_en, item.name_sv)}
+                    </Typography>
+                    <ButtonBase onClick={() => closePopup()} className={classes.popupCloseButton}>
+                      <Close className={classes.infoIcon} />
+                    </ButtonBase>
+                  </div>
+                  <div className={classes.content}>
+                    <Typography variant="body2">
+                      {renderDescription(
+                        item.description,
+                        item.description_en,
+                        item.description_sv,
+                        item.name,
+                        item.name_en,
+                        item.name_sv,
+                      )}
                     </Typography>
                   </div>
                 </div>
--- src/components/MobilityPlatform/CultureRouteUnits/styles.js
+++ src/components/MobilityPlatform/CultureRouteUnits/styles.js
@@ -1,9 +1,22 @@
 export default theme => ({
   popupInner: {
-    margin: theme.spacing(1.5),
+    padding: theme.spacing(1.5),
   },
-  subtitle: {
-    width: '88%',
+  header: {
+    width: '98%',
     marginBottom: theme.spacing(1),
+    borderBottom: '1px solid rgba(0, 0, 0, 255)',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  popupCloseButton: {
+    padding: theme.spacing(0.8),
+    '&:hover': {
+      cursor: 'pointer',
+      borderRadius: '5px',
+      border: '1px solid rgba(0, 0, 0, 255)',
+    },
   },
 });
