d078594a1f6cc4b1af21850e0a8815d7cee948ed
juhomakkonen <juho.makkonen@turku.fi>
2022-06-28 15:03:10 +0300

    Move SMLogo to own component to add unit test for it

---
+++ src/components/TopBar/SMLogo/index.js
@@ -0,0 +1,7 @@
+import { withStyles } from '@material-ui/core';
+import SMLogoComponent from './SMLogoComponent';
+import styles from '../styles';
+
+const SMLogo = withStyles(styles)(SMLogoComponent);
+
+export default SMLogo;
