dae8b65cbc458b85c628502f72cfc7caaa81f65a
juhomakkonen <juho.makkonen@turku.fi>
2022-09-16 12:53:54 +0300

    Add unit tests for scooter info -modal

---
+++ src/components/MobilityPlatform/Scooters/components/ScooterMarkers/components/ScooterInfo/__tests__/ScooterInfo.test.js
@@ -0,0 +1,46 @@
+// Link.react.test.js
+import React from 'react';
+import { getRenderWithProviders } from '../../../../../../../../../jestUtils';
+import finnishTranslations from '../../../../../../../../i18n/fi';
+import ScooterInfo from '../index';
+
+const mockProps = {
+  item: {
+    bike_id: 'abc123test',
+    is_reserved: false,
+    current_range_meters: 20200,
+    rental_uris: {
+      android: 'https://www.testilinkki.fi',
+      ios: 'https://www.testilinkki.fi',
+    },
+  },
+};
+
+const renderWithProviders = getRenderWithProviders({});
+
+describe('<ScooterInfo />', () => {
+  it('should work', () => {
+    const { container } = renderWithProviders(<ScooterInfo {...mockProps} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('does show text correctly', () => {
+    const { container } = renderWithProviders(<ScooterInfo {...mockProps} />);
+
+    const p = container.querySelectorAll('p');
+    const h6 = container.querySelector('h6');
+    expect(h6.textContent).toContain(finnishTranslations['mobilityPlatform.content.scooter.title']);
+    expect(p[0].textContent).toContain(`${finnishTranslations['mobilityPlatform.content.general.provider']}: Ryde`);
+    expect(p[1].textContent).toContain(finnishTranslations['mobilityPlatform.content.scooter.notReserved']);
+    expect(p[2].textContent).toContain(`${finnishTranslations['mobilityPlatform.content.scooter.range']}: 20.20 km`);
+    expect(p[3].textContent).toContain(finnishTranslations['mobilityPlatform.content.general.rentalUris']);
+  });
+
+  it('does render links correctly', () => {
+    const { container } = renderWithProviders(<ScooterInfo {...mockProps} />);
+
+    const link = container.querySelectorAll('a');
+    expect(link[0].textContent).toEqual('Android');
+    expect(link[1].textContent).toEqual('iOS');
+  });
+});
