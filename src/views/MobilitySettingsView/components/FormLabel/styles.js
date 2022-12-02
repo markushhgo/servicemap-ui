6e895f39f31947024b5b94d8bc296fb3b7169312
juhomakkonen <juho.makkonen@turku.fi>
2022-12-02 10:52:11 +0200

    Adjust padding values for mobile screens

--- src/views/MobilitySettingsView/components/FormLabel/styles.js
+++ src/views/MobilitySettingsView/components/FormLabel/styles.js
@@ -1,15 +1,14 @@
 const styles = {
-  formControl: {
-    width: '100%',
-  },
-  formGroup: {
-    marginTop: '0',
-  },
   formLabel: {
-    padding: '0.4rem 3rem',
     backgroundColor: 'rgba(70,72,75,255)',
     margin: '0',
     color: '#fff',
+  },
+  paddingMd: {
+    padding: '0.5rem 3rem',
+  },
+  paddingSm: {
+    padding: '0.5rem 0.8rem',
   },
 };
 
