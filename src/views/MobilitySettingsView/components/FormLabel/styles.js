18fb60a6986bbc87fc745fe1415db13499d0aedf
juhomakkonen <juho.makkonen@turku.fi>
2022-12-02 15:01:05 +0200

    Change to left & right padding values

--- src/views/MobilitySettingsView/components/FormLabel/styles.js
+++ src/views/MobilitySettingsView/components/FormLabel/styles.js
@@ -2,9 +2,14 @@ const styles = {
   formLabel: {
     backgroundColor: 'rgba(70,72,75,255)',
     margin: '0',
-    padding: '0.5rem 3rem',
     color: '#fff',
     textAlign: 'left',
+  },
+  paddingMd: {
+    padding: '0.4rem 3rem',
+  },
+  paddingSm: {
+    padding: '0.4rem 1rem',
   },
 };
 
